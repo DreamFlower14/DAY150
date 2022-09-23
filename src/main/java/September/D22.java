c60a9f6300cc45ad4fcb373552b9f78823494bc9
DreamFlower14 <sunghh1004@naver.com>
2022-09-23 14:05:13 +0900

    220922 CodeUp 1733

--- src/main/java/September/D22.java
+++ src/main/java/September/D22.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class D22 {
     public static void main(String[] args) {
+        // CodeUp 1419
         Scanner scan = new Scanner(System.in);
 
         String love = scan.nextLine();
---
+++ src/main/java/September/D23.java
@@ -0,0 +1,18 @@
+package September;
+
+import java.util.Scanner;
+
+public class D23 {
+        // CodeUp 1733
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String IOI = scan.nextLine();
+
+        if (IOI.equals("IOI")){
+            System.out.println("IOI is the International Olympiad in Informatics.");
+        }else {
+            System.out.println("I don't care.");
+        }
+    }
+}
