ccb2afade41bc9c01cc02550a5e52fd29ed1c285
DreamFlower14 <sunghh1004@naver.com>
2022-10-05 18:24:19 +0900

    221004 백준 2275 부녀회장이 될테야 실패

--- src/main/java/October/D04.java
+++ src/main/java/October/D04.java
@@ -7,13 +7,12 @@ public class D04 {
         Scanner scan = new Scanner(System.in);
 
         int num = scan.nextInt();  // 입력 받은 n번째 수
-        int i = 1;
+        int i = 1; // 집합의 개수 ex)
         int u; // 분자
         int d; // 분모
 
         while(true){
             if ((num - i) < 1){
-                //i++;
                 break;
             }
             num -= i;
@@ -24,8 +23,8 @@ public class D04 {
             u = i - (num - 1);
             d = num;
         }else {             // 짝수 일 때
-            u = num;
-            d = i - (num - 1);
+            u = num; // 오름차순
+            d = i - (num - 1); // 내림차순
         }
 
         System.out.println( u + "/" + d);
---
+++ src/main/java/October/D05.java
@@ -0,0 +1,9 @@
+package October;
+
+public class D05 {
+    public static void main(String[] args) {
+        // 백준 2775 부녀회장이 될테야
+
+    }
+
+}
