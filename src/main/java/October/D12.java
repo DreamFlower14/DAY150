7901964ecec740ddab123eaa2d116176373d1cd6
DreamFlower14 <sunghh1004@naver.com>
2022-10-19 23:16:07 +0900

    221019

--- src/main/java/October/D12.java
+++ src/main/java/October/D12.java
@@ -2,8 +2,10 @@ package October;
 
 import java.util.Scanner;
 
+import static java.lang.Math.*;
+
 /**
- * 날짜 : 2022-10-13
+ * 날짜 : 2022-10-18
  * 시간 : 오후 12:09
  */
 public class D12 {
@@ -12,8 +14,10 @@ public class D12 {
         System.out.println(solution(2,10));
     }
 
+    double a = pow(1,5);
+
     public static int solution(int n, int t) {
-            int b = (int) Math.pow(2,t);
+            int b = (int) pow(2,t);
             int answer = b * n;
             return answer;
     }
