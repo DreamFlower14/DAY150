b5ec43a3b4db06e34904695d641dca32dbeae979
DreamFlower14 <sunghh1004@naver.com>
2022-10-21 10:21:06 +0900

    221021 프로그래머스 문자열 정수변환

---
+++ src/main/java/October/D21.java
@@ -0,0 +1,11 @@
+package October;
+
+public class D21 {
+    public static void main(String[] args) {
+        System.out.println(solution("12345"));
+    }
+    public static int solution(String s) {
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+}
