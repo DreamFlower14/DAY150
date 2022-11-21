9cf526faf752e4d9096ec35c56d8e233c9b57f36
DreamFlower14 <sunghh1004@naver.com>
2022-11-21 19:53:12 +0900

    221121 문자열 정렬

---
+++ src/main/java/November/D21.java
@@ -0,0 +1,21 @@
+package November;
+
+import java.util.Arrays;
+
+public class D21 {
+    public static void main(String[] args) {
+        D21 D = new D21();
+        System.out.print(D.solution());
+
+    }
+    public String solution(String s) {
+        String answer = "";
+
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        answer = new String(chars);
+
+        return answer;
+    }
+
+}
