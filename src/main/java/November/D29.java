f0f7e65087777daa8c40d6bbaa1882b1be7de1cc
DreamFlower14 <sunghh1004@naver.com>
2022-11-29 08:54:09 +0900

    221129 프로그래머스 올바른 괄호

---
+++ src/main/java/November/D29.java
@@ -0,0 +1,24 @@
+package November;
+
+public class D29 {
+    public static void main(String[] args) {
+        // 프로그래머스 올바른 괄호
+        D29 D = new D29();
+        System.out.print(D.solution("(()("));
+
+    }
+    boolean solution(String s) {
+        int cnt = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                cnt++;
+            }else{
+                cnt--;
+                if(cnt < 0)
+                    return false;
+            }
+        }
+        if(cnt == 0){return true;}else{return false;}
+    }
+
+}
