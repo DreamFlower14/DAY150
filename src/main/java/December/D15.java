fd773b26e2cd405fd8eeb8f9714bbd92e3130aa9
DreamFlower14 <sunghh1004@naver.com>
2022-12-15 21:06:15 +0900

    221215

---
+++ src/main/java/December/D15.java
@@ -0,0 +1,48 @@
+package December;
+
+public class D15 {
+    public static void main(String[] args) {
+        D15 D = new D15();
+//        System.out.print(D.solution());
+
+    }
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        int[] people = new int[n];
+
+        //(1)
+        for(int i: lost) {
+            people[i-1]--;
+        }
+
+        //(2)
+        for(int j: reserve) {
+            people[j-1]++;
+        }
+
+
+        for(int i=0; i<people.length;i++) {
+            //(3)
+            if(people[i] == -1) {
+                //(4)
+                if(i>0 && people[i-1] == 1) {
+                    people[i]++;
+                    people[i-1]--;
+                    //(5)
+                }else if(i < people.length-1 && people[i+1] ==1) {
+                    people[i]++;
+                    people[i+1]--;
+                }
+            }
+        }
+        //(6)
+        for(int i : people) {
+            if(i == 0) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+}
