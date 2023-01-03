40d7f55e7c29cef291cad9d0749b08b86843758c
DreamFlower14 <sunghh1004@naver.com>
2023-01-03 19:34:49 +0900

    230103

--- src/main/java/January/D02_2.java
+++ src/main/java/January/D02_2.java
@@ -2,7 +2,9 @@ package January;
 
 public class D02_2 {
     public static void main(String[] args) {
-        // 프로그래머스 
+        // 프로그래머스 문자 반복 출력하기 [+3]
+        // 문자열 my_string과 정수 n이 매개변수로 주어질 때,
+        // my_string에 들어있는 각 문자를 n만큼 반복한 문자열을 return 하도록 solution 함수를 완성해보세요.
 
         D02_2 D = new D02_2();
         System.out.print(D.solution("hi",2));
@@ -11,7 +13,9 @@ public class D02_2 {
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < my_string.length(); i++) {
-            sb.append(my_string.charAt(i));
+            for (int j = 0; j < n; j++) {
+                sb.append(my_string.charAt(i));
+            }
         }
         return sb.toString();
     }
