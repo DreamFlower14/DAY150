c98d08c7a1fbae8f875885a6adb0eed7a69e04d4
DreamFlower14 <sunghh1004@naver.com>
2023-01-04 23:33:32 +0900

    230104

---
+++ src/main/java/January/D04.java
@@ -0,0 +1,19 @@
+package January;
+
+import java.util.Scanner;
+
+/**
+ * 날짜 : 2023-01-04
+ * 시간 : 오후 11:27
+ */
+public class D04 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while(true){
+            int n = scan.nextInt();
+            if (n == 0){
+                break;
+            }
+        }
+    }
+}
