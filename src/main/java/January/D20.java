8f3eb3794da96c4a2e22e5740d0827aaaa7d1ce8
DreamFlower14 <sunghh1004@naver.com>
2023-01-23 22:22:11 +0900

    230123

--- src/main/java/January/D20.java
+++ src/main/java/January/D20.java
@@ -2,7 +2,7 @@ package January;
 
 public class D20 {
     public static void main(String[] args) {
-        // 프로그래머스 합성수 찾기
+        // 프로그래머스 합성수 찾기 [ +1 ]
 
         D20 D = new D20();
         System.out.print(D.solution(10));
@@ -12,15 +12,16 @@ public class D20 {
         int answer = 0;
         int cnt = 0;
 
-        for(int i = 2; i <= n; i++){
+        for(int i = 4; i <= n; i++){
             cnt = 0;
-            for(int j = 1; j < i; j++){
-                if( i % j == 0 ){
+            for(int j = 1; j <= i; j++){
+                if( i % j == 0 ){   // 약수면 cnt 를 하나 올린다.
                     cnt++;
+                    System.out.println("i : " + i + " j : " + j);
                 }
 
             }
-            if(cnt >= 3){
+            if(cnt >= 3){   // 약수의 개수가 3개 이상이면 answer 를 하나 올린다.
                 answer++;
             }
         }
