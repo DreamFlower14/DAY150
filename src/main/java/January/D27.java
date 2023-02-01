27d3a42be89e29bfc7de888425132563710cffb5
DreamFlower14 <sunghh1004@naver.com>
2023-02-01 23:24:54 +0900

    230201

--- src/main/java/January/D27.java
+++ src/main/java/January/D27.java
@@ -9,11 +9,11 @@ public class D27 {
         // my_string 안에 있는 숫자만 골라 오름차순 정렬한 리스트를 return 하도록 solution 함수를 작성해보세요.
 
         D27 D = new D27();
-        System.out.print(D.solution("hi12392"));
+        System.out.print(D.solution("abcde0"));
     }
 
     public int[] solution(String my_string) {
-        my_string = my_string.replaceAll("[^1-9]", "");
+        my_string = my_string.replaceAll("[^0-9]", "");
         System.out.println(my_string);
 
         int[] answer = new int[my_string.length()];
