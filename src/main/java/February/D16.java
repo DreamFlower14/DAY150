6ac39858aca8de3b455e653054e124f28a6820ac
DreamFlower14 <sunghh1004@naver.com>
2023-02-16 23:03:33 +0900

    230215

---
+++ src/main/java/February/D16.java
@@ -0,0 +1,16 @@
+package February;
+
+public class D16 {
+    public static void main(String[] args) {
+        // 프로그래머스 
+
+        D16 D = new D16();
+        System.out.print(D.solution(2,3));
+    }
+
+    public static int solution(int a, int b){
+        int answer = a + b;
+
+        return answer;
+    }
+}
